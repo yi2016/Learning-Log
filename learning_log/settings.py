a0b27302d3a7f5ee4a921ddb4b0148fcaacb84a7
yi <wuyi1989@outlook.com>
2018-01-02 16:21:07 +0800

    modify settings.py

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT='/static'
 
 #我的设置
 LOGIN_URL='/users/login'
@@ -133,6 +134,8 @@ LOGIN_URL='/users/login'
 BOOTSTRAP3={
     'include_jquery':True,
 }
+
+
 
 #Heroku设置
 if os.getcwd()=='/app':
