032e89abbbbd1e31d742e4d97d368d422f32a335
yi <wuyi1989@outlook.com>
2018-01-02 16:03:54 +0800

    update

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -146,8 +146,4 @@ if os.getcwd()=='/app':
     ALLOWED_HOSTS=['*']
 
     BASE_DIR=os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT='staticfiles'
-
-    STATICFILES_DIRS=(
-        os.path.join(BASE_DIR,'static'),
-    )
+    STATIC_ROOT=os.path.join(BASE_DIR,'static')
